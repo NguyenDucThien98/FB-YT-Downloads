86006dd2ac9213161a17c348a2c9e0efcbaf9a58
NguyenDucThien98 <39368428+NguyenDucThien98@users.noreply.github.com>
2019-02-19 15:57:21 +0700

    update

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -27,6 +27,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Download2 = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,15 +47,15 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(47, 47);
+            this.textBox1.Location = new System.Drawing.Point(94, 47);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(268, 20);
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Location = new System.Drawing.Point(54, 50);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 2;
@@ -65,11 +71,70 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Facebook/Youtube Download";
             // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(94, 73);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(302, 20);
+            this.txtRegex.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Regex :";
+            // 
+            // Download2
+            // 
+            this.Download2.Location = new System.Drawing.Point(321, 97);
+            this.Download2.Name = "Download2";
+            this.Download2.Size = new System.Drawing.Size(75, 23);
+            this.Download2.TabIndex = 8;
+            this.Download2.Text = "Download";
+            this.Download2.UseVisualStyleBackColor = true;
+            this.Download2.Click += new System.EventHandler(this.Download2_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(240, 97);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 9;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status  :";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(94, 106);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 13);
+            this.status.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 87);
+            this.ClientSize = new System.Drawing.Size(408, 216);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Download2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRegex);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -87,6 +152,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Download2;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label status;
     }
 }
 
--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -4,8 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,12 +22,49 @@ namespace WindowsFormsApplication2 {
             string link = textBox1.Text;
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-          //  startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //  startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = "/C youtube-dl "+link;
+            startInfo.Arguments = "/C youtube-dl " + link;
             process.StartInfo = startInfo;
             process.Start();
 
+        }
+        Boolean check = true;
+        string html;
+        private void Download2_Click(object sender,EventArgs e) {
+            check = true;
+            OpenFileDialog o = new OpenFileDialog();
+            if (o.ShowDialog() == DialogResult.OK) {
+                html = File.ReadAllText(o.FileName);
+                Thread t = new Thread(threadDow);
+                t.Start();
+            }
+        }
+        void threadDow() {
+            string regex = txtRegex.Text;
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            Regex reg = new Regex(regex);
+            int count = 0;
+            Match match = reg.Match(html);
+            do {
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C youtube-dl https://www.facebook.com" + match.ToString();
+                process.StartInfo = startInfo;
+                process.Start();
+                MethodInvoker inv = delegate {
+                    this.status.Text = count + "/" + match.Length;
+                };
+                this.Invoke(inv);
+                process.WaitForExit();
+                match = match.NextMatch();
+                count++;
+            } while (match != Match.Empty && check == true);
+        }
+
+        private void Stop_Click(object sender,EventArgs e) {
+            check = false;
         }
     }
 }
